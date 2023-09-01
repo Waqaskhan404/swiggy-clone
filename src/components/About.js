5db7bc7d6dd2758581cae8c99e458d55362eb4bc
Waqas Khan <71628657+Waqaskhan404@users.noreply.github.com>
2023-09-02 00:41:06 +0500

    class Component Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import ClassBaseComponent from "./ClassBaseComponent";
+
 const About=()=>{
     return(
         <div>
             <h1>About  Us</h1>
             <p>This is About Us page</p>
+
+            <ClassBaseComponent/>
 
         </div>
     )
