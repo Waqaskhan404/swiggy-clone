6e6c442b78da2047c400cab533852ceea30828ac
Waqas Khan <71628657+Waqaskhan404@users.noreply.github.com>
2023-09-02 03:08:16 +0500

    Lazy Loading / Dynamic Routing Chunking ,making bundler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Children } from "react";
+import React, { Children, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { Footer } from "./components/Footer";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-import About from "./components/About"
+// import About from "./components/About"
 import { lazy } from "react";
 import Contact from "./components/Contact"
 import Error from "./components/Error";
@@ -18,8 +18,11 @@ import RestaurantMenu from "./components/RestaurantMenu";
 
 
 
+const About=lazy(()=> import("./components/About"))
+
 
 const AppLayout = () => {
+
   return (
     <>
       <Header />
@@ -39,9 +42,13 @@ const appRoute=createBrowserRouter([
         path:"/",
         element:<Body/>
       },
+      // {
+      //   path:"/about",
+      //   element:<About/>
+      // },
       {
         path:"/about",
-        element:<About/>
+        element:<Suspense><About/></Suspense>
       },
       {
         path:"/contact",
